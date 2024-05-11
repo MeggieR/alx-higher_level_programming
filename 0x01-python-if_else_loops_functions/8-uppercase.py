71972e401f6f7a6724b4141746f1c517559c9cba
MeggieR <mrotrigs@gmail.com>
2024-05-11 07:58:31 -0700

    loops

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for itt in str:
+        temporary = itt
+        if ord(temporary) >= 97 and ord(temporary) <= 122:
+            temporary = chr(ord(temporary) - 32)
+        print()
