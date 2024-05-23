8c4bfcb6daafaeb140f3470d91e681d9217d1328
MeggieR <mrotrigs@gmail.com>
2024-05-22 18:06:12 -0700

    correction

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 --  Show the grants for specific users in MySQL
-SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
-SHOW GRANTS FOR 'user_0d_2' @ 'LOCALHOST';
+--  Show the grants for specific users in MySQL
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
