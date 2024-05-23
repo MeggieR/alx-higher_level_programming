8bb80330c5a89612a6ccc1b5e1e0f0551e34cecd
MeggieR <mrotrigs@gmail.com>
2024-05-22 18:52:33 -0700

    m

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,7 +1,5 @@
--- Create the table 'force_name' if it does not already exist
--- ID: an integer column for unique identifier
--- name: a VARCHAR column with a maximum length of 256 characters, cannot be NULL
+-- Create the table 'force_name' if it does not already e
 CREATE TABLE IF NOT EXISTS force_name (
-    ID INT, 
+    ID INT,
     name VARCHAR(256) NOT NULL
 );
